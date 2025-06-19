be8abf9af18abe24ac89266c47eaa480f4b87e97
AmikMain <amikmain001@gmail.com>
2025-06-19 12:00:13 +0400

    Selection Box [INVISIBLE]
    It is invisible...
    But its working!

--- Assets/Scripts/Units/ClickSelector.cs
+++ Assets/Scripts/Units/ClickSelector.cs
@@ -1,18 +1,65 @@
 using UnityEngine;
 using Unity.Netcode;
+using Unity.VisualScripting;
+using System;
 
 public class ClickSelector : NetworkBehaviour
 {
     public SelectedList selectedList;
+    Vector2 startPosition = Vector2.zero;
 
     void Update()
     {
         ThrowRay();
+
+        DrawSelectionBox();
+    }
+
+    private void DrawSelectionBox()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (startPosition == Vector2.zero) return;
+
+            if (!InputManager.Instance.IsShiftPressed())
+            {
+                // If shift is not pressed, clear the selection list
+                foreach (GameObject unit in selectedList.selectedList)
+                {
+                    unit.GetComponent<Unit>().IsSelected = false;
+                }
+                selectedList.selectedList.Clear();
+            }
+
+            Collider2D[] colliders = Physics2D.OverlapAreaAll(startPosition, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.CompareTag("Unit"))
+                {
+                    if (!collider.GetComponent<NetworkBehaviour>().IsOwner) return;
+
+                    Unit unitComponent = collider.GetComponent<Unit>();
+
+                    collider.GetComponent<Unit>().IsSelected = true;
+                    if (!selectedList.selectedList.Contains(collider.gameObject))
+                    {
+                        selectedList.selectedList.Add(collider.gameObject);
+                    }
+                }
+            }
+        }
     }
 
     private void ThrowRay()
     {
-         if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hit.collider != null)
